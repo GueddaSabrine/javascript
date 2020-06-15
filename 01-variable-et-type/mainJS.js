7dc3df021ee128e8a7ee755b7a27b9d4b4c731cf
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2020-06-15 16:08:12 +0200

    commit new

---
+++ 01-variable-et-type/mainJS.js
@@ -0,0 +1,27 @@
+//declaration de variables
+/*
+Ceci est un commentaire
+ */
+
+//on declare la variable maVariable
+let maVariable;
+
+/*deteremination du type de la variable avec typeof*/
+typeof maVariable;
+console.log(typeof maVariable);
+
+//on fait une assignation ou une affectation
+maVariable=25;
+console.log(typeof maVariable);//number
+
+maVariable= "sabrine";
+console.log((typeof maVariable));//string
+
+maVariable= true;
+console.log(typeof maVariable);//boolean
+
+maVariable= null;
+console.log(typeof maVariable);//object
+
+
+
