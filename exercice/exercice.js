fe3ac3d8559a81e01d97ef8e97ca42abac0a0899
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2020-06-16 14:23:04 +0200

    comiit exo

---
+++ exercice/exercice.js
@@ -0,0 +1,20 @@
+//je declare mes variable
+/*let a , b, c;
+a = prompt(message "A =5");
+b = prompt(message "B =13");
+console.log(`Avant permutation`);
+console.log(`a = ${a}`);
+console.log(`b = ${b}`)
+//On effecture la permutation
+c = a;
+a = b;
+b = c;
+console.log(`Après permutation`);
+console.log(`a = ${a}`);
+console.log(`b = ${b}`)
+ */
+
+let a, b;
+a = prompt(message "entrez un nombre");
+console.log(`a = ${b}`)
+b = a * a;
