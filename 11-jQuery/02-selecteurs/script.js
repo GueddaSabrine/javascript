a06957bf23dc5ec264693a861087f9cabfd59802
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2020-06-25 11:56:54 +0200

    commit to jQuery

---
+++ 11-jQuery/02-selecteurs/script.js
@@ -0,0 +1,33 @@
+//Les selecteurs en jQuery
+
+$(document).ready(function(){
+
+    //Cibler un element HTML
+    // $("button").click(function () {
+    //     $("p").hide();
+    // })
+
+    // $("button").click(function(){
+    //     $(".maClasse").hide();
+    // });
+
+    // $("button").click(function(){
+    //     $("*").hide()
+    // });
+
+    // $("button").click(function(){
+    //     $(".test").hide()
+    // });
+
+    // $("button").click(function(){
+    //     $("p:first").hide()
+    // });
+
+    // $("button").click(function(){
+    //     $("ul:first").hide()
+    // });
+
+
+
+
+});
